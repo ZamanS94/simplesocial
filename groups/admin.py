d0214e568bc96a114094bd17c6cc930cd4eab4fe
ZS94 <sabina.zaman94@gmail.com>
2026-06-07 00:59:01 +0300

    just admin related task

--- groups/admin.py
+++ groups/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-
 from . import models
 
 
 class GroupMemberInline(admin.TabularInline):
     model = models.GroupMember
+    extra = 1
 
 
+class GroupAdmin(admin.ModelAdmin):
+    inlines = [GroupMemberInline]
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
 
-admin.site.register(models.Group)
+
+admin.site.register(models.Group, GroupAdmin)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("user", "group", "created_at")
+    list_filter = ("group", "created_at")
+    search_fields = ("message",)
+
+
+admin.site.register(Post, PostAdmin)
